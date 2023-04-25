56041d5a07ae26106f2386994151779cfe1259c3
Vamshi2113 <panasakrishna@jklu.edu.in>
2023-04-25 18:55:09 +0530

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express= require("express");
 const app=express();
 const path=require("path")
 const port=80;
-
+const mongoose=require('mongoose')
 
 
 
@@ -11,32 +11,69 @@ app.use("/static",express.static('static'));
 app.set("view engine","pug");
 app.set('views',path.join(__dirname,'views'));
 
+
+
+main().catch(err => console.log(err)
+);
+async function main() {
+  mongoose.set('strictQuery', true);
+  await mongoose.connect('mongodb+srv://vamshi:Vv72177217@cluster0.9cfah0p.mongodb.net/?retryWrites=true&w=majority',{
+    useUnifiedTopology: true
+  });
+  console.log("connected");
+  
+  // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
+}
+var db=mongoose.connection;
+db.once('open',function(){
+  console.log("connected...................;;");
+})
+
+
+
+
+
+var phoneschema = new mongoose.Schema({
+    name: String, 
+    phone:String
+  });
+  
+var phonemodel = mongoose.model('phone',phoneschema);
+  
+
+
+
+
+
+
+
+
 app.get("/",(req,res)=>{
     res.render("home.pug");
 })
 
-app.get("/home",(req,res)=>{
+app.get("/home",async(req,res)=>{
     console.log("recieved home.get")
-    res.render("dashboard.pug")
 
-})
-app.post("/home",(req,res)=>{
-    console.log("recieved")
-    res.render("dashboard.pug")
+    let data= await phonemodel.find({},{});
+    console.log("data=",data)
+    let params={numbers:data};
+
+    res.render("dashboard.pug",params)
 
 })
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.post("/save_number",(req,res)=>{
-    var form_data=req.body;
-    console.log(form_data);
+app.post("/save_number",async (req,res)=>{
+    var form_data={name:req.body.NAME,phone:req.body["MOBILE NUMBER"]};
+    console.log(form_data)
+
+    var phonedata = new phonemodel(form_data);   
+    console.log("phn model==>",phonemodel,"===end===")
+    await phonedata.save();
+    res.redirect("/home")
 })
-
-
-
-
-
 
 
 
---
+++ static/dashboard.js
@@ -0,0 +1,5 @@
+let form_submit_btn=document.getElementsByClassName("form_submit_btn")[0];
+// form_submit_btn.addEventListener("click",(event)=>{
+//     event.preventDefault();
+
+// })
